308c996208a703e166716cbdb4f6f99ab0b92f36
ttrinh3 <timothytrinh4295@gmail.com>
2022-06-26 00:00:07 -0700

    FINALLY

--- main/main.c
+++ main/main.c
@@ -1,99 +1,3 @@
-
-
-/**
- * Simple example with one sensor connected either to I2C bus 0 or
- * SPI bus 1.
- *
- * Harware configuration:
- *
- *   I2C
- *
- *   +-----------------+   +----------+
- *   | ESP8266 / ESP32 |   | BME680   |
- *   |                 |   |          |
- *   |   GPIO 14 (SCL) ----> SCL      |
- *   |   GPIO 13 (SDA) <---> SDA      |
- *   +-----------------+   +----------+
- *
- *   SPI   
- *
- *   +-----------------+   +----------+      +-----------------+   +----------+
- *   | ESP8266         |   | BME680   |      | ESP32           |   | BME680   |
- *   |                 |   |          |      |                 |   |          |
- *   |   GPIO 14 (SCK) ----> SCK      |      |   GPIO 16 (SCK) ----> SCK      |
- *   |   GPIO 13 (MOSI)----> SDI      |      |   GPIO 17 (MOSI)----> SDI      |
- *   |   GPIO 12 (MISO)<---- SDO      |      |   GPIO 18 (MISO)<---- SDO      |
- *   |   GPIO 2  (CS)  ----> CS       |      |   GPIO 19 (CS)  ----> CS       |
- *   +-----------------+    +---------+      +-----------------+   +----------+
- */
-
-/* -- use following constants to define the example mode ----------- */
-
-// #define SPI_USED
-
-/* -- includes ----------------------------------------------------- */
-
-#include "bme680.h"
-#include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "pms5003.h"
-
-/* -- platform dependent definitions ------------------------------- */
-
-#ifdef ESP_PLATFORM  // ESP32 (ESP-IDF)
-
-// user task stack depth for ESP32
-#define TASK_STACK_DEPTH 4096
-
-// SPI interface definitions for ESP32
-#define SPI_BUS       HSPI_HOST
-#define SPI_SCK_GPIO  16
-#define SPI_MOSI_GPIO 17
-#define SPI_MISO_GPIO 18
-#define SPI_CS_GPIO   19
-
-#else  // ESP8266 (esp-open-rtos)
-
-// user task stack depth for ESP8266
-#define TASK_STACK_DEPTH 2048
-
-// SPI interface definitions for ESP8266
-#define SPI_BUS       1
-#define SPI_SCK_GPIO  14
-#define SPI_MOSI_GPIO 13
-#define SPI_MISO_GPIO 12
-#define SPI_CS_GPIO   2   // GPIO 15, the default CS of SPI bus 1, can't be used
-
-#endif  // ESP_PLATFORM
-
-// I2C interface defintions for ESP32 and ESP8266
-#define I2C_BUS       0
-#define I2C_SCL_PIN   14
-#define I2C_SDA_PIN   13
-#define I2C_FREQ      I2C_FREQ_100K
-
-/* -- user tasks --------------------------------------------------- */
-
-static bme680_sensor_t* sensor = 0;
-
-/*
- * User task that triggers measurements of sensor every seconds. It uses
- * function *vTaskDelay* to wait for measurement results. Busy wating
- * alternative is shown in comments
- */
-void user_task(void *pvParameters)
-{
-  
-}
-
-/* -- main program ------------------------------------------------- */
-
-
-
-
-
-
 #include <stdio.h>
 #include <string.h>
 #include "freertos/FreeRTOS.h"
@@ -107,7 +11,20 @@ void user_task(void *pvParameters)
 #include "cJSON.h"
 #include "connect.h"
 #include "mqtt_client.h"
-#include "esp8266_wrapper.h"
+#include "bme680.h"
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "pms5003.h"
+
+#define I2C_BUS       0
+#define I2C_SCL_PIN   14
+#define I2C_SDA_PIN   13
+#define I2C_FREQ      I2C_FREQ_100K
+
+/* -- user tasks --------------------------------------------------- */
+
+static bme680_sensor_t* sensor = 0;
 
 #define TAG "MQTT"
 
@@ -175,9 +92,16 @@ void MQTTLogic(int sensorReading)
       esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, client);
       esp_mqtt_client_start(client);
       break;
-    case MQTT_CONNECTED:;
+    case MQTT_CONNECTED:
+      esp_mqtt_client_subscribe(client, "/topic/my/subscription/1", 2);
+      char data[50];
+      sprintf(data, "%d", sensorReading);
+      printf("sending data: %d", sensorReading);
 
-      pms5003_config_t pms0 = {
+
+
+
+       pms5003_config_t pms0 = {
         .set_pin = GPIO_NUM_13,
         .reset_pin = GPIO_NUM_27,
         .mode_pin = GPIO_NUM_26,
@@ -188,19 +112,30 @@ void MQTTLogic(int sensorReading)
     };
     pms5003_setup(&pms0);
     // Set UART Parameter.
-    
+    uart_set_baud(0, 115200);
     // Give the UART some time to settle
     vTaskDelay(1);
 
-    
 
+    /** -- MANDATORY PART -- */
 
-   
+    #ifdef SPI_USED
+
+    spi_bus_init (SPI_BUS, SPI_SCK_GPIO, SPI_MISO_GPIO, SPI_MOSI_GPIO);
+
+    // init the sensor connected to SPI_BUS with SPI_CS_GPIO as chip select.
+    sensor = bme680_init_sensor (SPI_BUS, 0, SPI_CS_GPIO);
+
+    #else  // I2C
+
+    // Init all I2C bus interfaces at which BME680 sensors are connected
+    i2c_init(I2C_BUS, I2C_SCL_PIN, I2C_SDA_PIN, I2C_FREQ);
 
     // init the sensor with slave address BME680_I2C_ADDRESS_2 connected to I2C_BUS.
     sensor = bme680_init_sensor (I2C_BUS, BME680_I2C_ADDRESS_2, 0);
 
-     bme680_values_float_t values;
+    #endif  // SPI_USED
+
     if (sensor)
     {
         /** -- SENSOR CONFIGURATION PART (optional) --- */
@@ -218,7 +153,7 @@ void MQTTLogic(int sensorReading)
 
         // Set ambient temperature to 10 degree Celsius
         bme680_set_ambient_temperature (sensor, 10);
-            
+
         /** -- TASK CREATION PART --- */
 
         // must be done last to avoid concurrency situations with the sensor 
@@ -226,65 +161,51 @@ void MQTTLogic(int sensorReading)
 
         // Create a task that uses the sensor
         //start while loop here
+    bme680_values_float_t values;
 
+    TickType_t last_wakeup = xTaskGetTickCount();
 
-            TickType_t last_wakeup = xTaskGetTickCount();
+    // as long as sensor configuration isn't changed, duration is constant
+    uint32_t duration = bme680_get_measurement_duration(sensor);
 
-            // as long as sensor configuration isn't changed, duration is constant
-            uint32_t duration = bme680_get_measurement_duration(sensor);
-
-            pms5003_measurement_t reading;
-
+    pms5003_measurement_t reading;
+    while (1)
+    {
         // trigger the sensor to start one TPHG measurement cycle
         if (bme680_force_measurement (sensor))
         {
             // passive waiting until measurement results are available
-            // vTaskDelay (duration);
+            vTaskDelay (duration);
 
             // alternatively: busy waiting until measurement results are available
-             while (bme680_is_measuring (sensor)) ;
+            // while (bme680_is_measuring (sensor)) ;
 
             // get the results and do something with them
-           
             if (bme680_get_results_float (sensor, &values)){
-                printf(" BME680 Sensor: %.2f °C, %.2f %%, %.2f hPa, %.2f Ohm\n",
+              char buffer[500];
+                sprintf(buffer," BME680 Sensor: %.2f °C, %.2f %%, %.2f hPa, %.2f Ohm\n",
                        values.temperature, values.humidity,
                        values.pressure, values.gas_resistance);
                         pms5003_make_measurement(&pms0, &reading);
                         pms5003_print_measurement(&reading);       
-                       
+esp_mqtt_client_publish(client, "topic/my/timothy/1", buffer, strlen(buffer), 2, false);
                        }
         }
         // passive waiting until 1 second is over
         vTaskDelayUntil(&last_wakeup, 1000 / portTICK_PERIOD_MS);
-    
-      }
+    }
+    }
+    else
+        printf("Could not initialize BME680 sensor\n");
 
-
-
-
-
-
-
-
-
-
-
-
-      esp_mqtt_client_subscribe(client, "/topic/my/subscription/1", 2);
-      char data[500];
-      sprintf(data, "BME680 Sensor: %.2f °C, %.2f %%, %.2f hPa, %.2f Ohm\n", values.temperature, values.humidity,
-                       values.pressure, values.gas_resistance);
-      printf("sending data: BME680 Sensor: %.2f °C, %.2f %%, %.2f hPa, %.2f Ohm\n", values.temperature, values.humidity,
-                       values.pressure, values.gas_resistance);
-      esp_mqtt_client_publish(client, "topic/my/timothy/1", data, strlen(data), 2, false);
+      
       break;
-     case MQTT_PUBLISHED:
+    case MQTT_PUBLISHED:
       esp_mqtt_client_stop(client);
       esp_mqtt_client_destroy(client);
       esp_wifi_stop();
       return;
-     default:
+    default:
       break;
     }
   }
@@ -292,6 +213,7 @@ void MQTTLogic(int sensorReading)
 
 void OnConnected(void *para)
 {
+
   while (true)
   {
     int sensorReading;
@@ -318,12 +240,86 @@ void generateReading(void *params)
 
 
 
+
+/**
+ * Simple example with one sensor connected either to I2C bus 0 or
+ * SPI bus 1.
+ *
+ * Harware configuration:
+ *
+ *   I2C
+ *
+ *   +-----------------+   +----------+
+ *   | ESP8266 / ESP32 |   | BME680   |
+ *   |                 |   |          |
+ *   |   GPIO 14 (SCL) ----> SCL      |
+ *   |   GPIO 13 (SDA) <---> SDA      |
+ *   +-----------------+   +----------+
+ *
+ *   SPI   
+ *
+ *   +-----------------+   +----------+      +-----------------+   +----------+
+ *   | ESP8266         |   | BME680   |      | ESP32           |   | BME680   |
+ *   |                 |   |          |      |                 |   |          |
+ *   |   GPIO 14 (SCK) ----> SCK      |      |   GPIO 16 (SCK) ----> SCK      |
+ *   |   GPIO 13 (MOSI)----> SDI      |      |   GPIO 17 (MOSI)----> SDI      |
+ *   |   GPIO 12 (MISO)<---- SDO      |      |   GPIO 18 (MISO)<---- SDO      |
+ *   |   GPIO 2  (CS)  ----> CS       |      |   GPIO 19 (CS)  ----> CS       |
+ *   +-----------------+    +---------+      +-----------------+   +----------+
+ */
+
+/* -- use following constants to define the example mode ----------- */
+
+// #define SPI_USED
+
+/* -- includes ----------------------------------------------------- */
+
+
+/* -- platform dependent definitions ------------------------------- */
+
+#ifdef ESP_PLATFORM  // ESP32 (ESP-IDF)
+
+// user task stack depth for ESP32
+#define TASK_STACK_DEPTH 4096
+
+// SPI interface definitions for ESP32
+#define SPI_BUS       HSPI_HOST
+#define SPI_SCK_GPIO  16
+#define SPI_MOSI_GPIO 17
+#define SPI_MISO_GPIO 18
+#define SPI_CS_GPIO   19
+
+#else  // ESP8266 (esp-open-rtos)
+
+// user task stack depth for ESP8266
+#define TASK_STACK_DEPTH 2048
+
+// SPI interface definitions for ESP8266
+#define SPI_BUS       1
+#define SPI_SCK_GPIO  14
+#define SPI_MOSI_GPIO 13
+#define SPI_MISO_GPIO 12
+#define SPI_CS_GPIO   2   // GPIO 15, the default CS of SPI bus 1, can't be used
+
+#endif  // ESP_PLATFORM
+
+// I2C interface defintions for ESP32 and ESP8266
+
+
+/*
+ * User task that triggers measurements of sensor every seconds. It uses
+ * function *vTaskDelay* to wait for measurement results. Busy wating
+ * alternative is shown in comments
+ */
+
+
+/* -- main program ------------------------------------------------- */
+
 void user_init(void)
 {
-   i2c_init(I2C_BUS, I2C_SCL_PIN, I2C_SDA_PIN, 100000);
-   uart_set_baud(0, 115200);
-  readingQueue = xQueueCreate(sizeof(int), 10);
+   readingQueue = xQueueCreate(sizeof(int), 10);
   wifiInit();
   xTaskCreate(OnConnected, "handel comms", 1024 * 5, NULL, 5, &taskHandle);
-  xTaskCreate(generateReading, "handel comms", 1024 * 5, NULL, 5, NULL);        
+  xTaskCreate(generateReading, "handel comms", 1024 * 5, NULL, 5, NULL);
 }
+
