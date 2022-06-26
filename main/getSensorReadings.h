1f417c8f09aa9dfcf1ba731cea14154736b15727
ttrinh3 <timothytrinh4295@gmail.com>
2022-06-25 22:59:46 -0700

    awefawefawef

---
+++ main/getSensorReadings.h
@@ -0,0 +1,37 @@
+#include "bme680.h"
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "pms5003.h"
+#ifdef ESP_PLATFORM  // ESP32 (ESP-IDF)
+
+// user task stack depth for ESP32
+#define TASK_STACK_DEPTH 4096
+
+// SPI interface definitions for ESP32
+#define SPI_BUS       HSPI_HOST
+#define SPI_SCK_GPIO  16
+#define SPI_MOSI_GPIO 17
+#define SPI_MISO_GPIO 18
+#define SPI_CS_GPIO   19
+
+#else  // ESP8266 (esp-open-rtos)
+
+// user task stack depth for ESP8266
+#define TASK_STACK_DEPTH 2048
+
+// SPI interface definitions for ESP8266
+#define SPI_BUS       1
+#define SPI_SCK_GPIO  14
+#define SPI_MOSI_GPIO 13
+#define SPI_MISO_GPIO 12
+#define SPI_CS_GPIO   2   // GPIO 15, the default CS of SPI bus 1, can't be used
+
+#endif  // ESP_PLATFORM
+
+// I2C interface defintions for ESP32 and ESP8266
+#define I2C_BUS       0
+#define I2C_SCL_PIN   14
+#define I2C_SDA_PIN   13
+#define I2C_FREQ      I2C_FREQ_100K
+void getSensorReadings(void);
